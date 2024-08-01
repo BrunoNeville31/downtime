f2e823cceb14d224289b70c5fd0945c0887bfbfd
brunoneville31 <brunoneville31@gmail.com>
2024-08-01 15:27:12 -0300

    Fixed commit

--- index.js
+++ index.js
@@ -3,11 +3,21 @@ const github = require('@actions/github');
 
 try {
   const daemons = core.getInput('selected_daemons');
-  console.log(`Hello ${daemons}!`);
+  const daemons_split = daemons.split(" ");
+  const first = []
+  const second = []
+  daemons_split.forEach(daemon => {
+    if(daemon.match("matching") || daemon.match("slavebook")){
+      let slavebook = daemon.replace("matching", "slavebook")
+      first.push(slavebook)
+    }else{
+      second.push(daemon)
+    }    
+  });
+
+  core.setOutput("daemons_first", first);
   
-  core.setOutput("daemons_first", daemons);
-  
-  core.setOutput("daemons_second", daemons);
+  core.setOutput("daemons_second", second);
 
 } catch (error) {
   core.setFailed(error.message);
