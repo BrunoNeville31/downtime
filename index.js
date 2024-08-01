a5836b6081302572872934bedf91ca8b1010e4bc
brunoneville31 <brunoneville31@gmail.com>
2024-08-01 15:08:12 -0300

    Fixed commit

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const daemons = core.getInput('selected_daemons');
+  console.log(`Hello ${daemons}!`);
+  
+  core.setOutput("daemons_first", daemons);
+  
+  core.setOutput("daemons_second", daemons);
+
 } catch (error) {
   core.setFailed(error.message);
 }
