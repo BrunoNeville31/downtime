cbc60ec7ff67bc359967aab7ee2747c02315e808
brunoneville31 <brunoneville31@gmail.com>
2024-08-05 17:19:28 -0300

    Fixed update action

--- index.js
+++ index.js
@@ -1,33 +1,63 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import core from '@actions/core';
 
-try {
-  const daemons = core.getInput('selected_daemons');
-  const daemons_split = daemons.split(" ");
-  const first = []
-  const second = []
-  daemons_split.forEach(daemon => {
-    if(daemon.match("matching") || daemon.match("slavebook")){
-      const slavebook = daemon.replace("matching", "slavebook")
-      
-      if(!first.includes(slavebook)){
-        first.push(slavebook)
-      }      
-      
-      if(daemon.match("matching")){
-        second.push(daemon)
-      }      
-    }else{
-      second.push(daemon)
-    }    
-  });
-
-  core.setOutput("daemons_first", first);
-  
-  core.setOutput("daemons_second", second);
-
-} catch (error) {
-  core.setFailed(error.message);
+const validate_daemon = (daemon) => {
+  if(daemon.length == 0){
+    return
+  }
 }
+
+const validate_build_daemons =  (build_daemons) => {
+  if(build_daemons.length == 0){
+    core.notice('No daemons to perform the build')
+  }
+}
+
+export const run = () => {
+  try {
+    const selected_daemons = core.getInput('selected_daemons').split(" ");
+    const step             = core.getInput('step');
+    const build_daemons    = []
+
+    if(step == "one"){
+      selected_daemons.forEach(daemon => {
+        validate_daemon(daemon)
+
+        if(daemon.match("matching")){          
+          const slavebook = daemon.replace("matching", "slavebook")
+
+          if(!build_daemons.includes(slavebook)){
+            build_daemons.push(slavebook)
+          } 
+        }
+      });
+
+      validate_build_daemons(build_daemons)
+    }else if(step == "two"){
+      selected_daemons.forEach(daemon => {
+        validate_daemon(daemon)
+
+        if(daemon.match("slavebook")){          
+          const matching = daemon.replace("slavebook", "matching")
+
+          if(!build_daemons.includes(matching)){
+            build_daemons.push(matching)
+          } 
+        }else{
+          if(!build_daemons.includes(daemon)){
+            build_daemons.push(daemon)
+          }
+        }
+      });
+
+      validate_build_daemons(build_daemons)
+    }
+
+    core.setOutput("daemons", build_daemons);    
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
 
 // matching-general-one slavebook-general-one trade-executor-general-one
