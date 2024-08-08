2ed2f2d6fe09cd761eb80415c3393a93074fb375
brunoneville31 <brunoneville31@gmail.com>
2024-08-08 11:29:34 -0300

    Fixed ruby validation

--- ruby/main.rb
+++ ruby/main.rb
@@ -4,5 +4,5 @@ puts "AQUIIIII"
 puts ARGV[0..1]
 
 
-#ENV["GITHUB_OUTPUT"] = "daemon=teste"
-system('echo "daemons=teste" >> $GITHUB_OUTPUT')
+ENV["GITHUB_OUTPUT"] = "daemons=teste"
+#system('echo "daemons=teste" >> $GITHUB_OUTPUT')
